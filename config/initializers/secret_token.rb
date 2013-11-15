8adb1d8f8a6c91dc305342d7bf029349edaf26b1
unknown <abradford@ITSM1033951.ACADEMIC.RRC.CA>
2013-11-14 21:00:45 -0600

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ComicNook::Application.config.secret_token = 'f32211c1216d6774fada3e259945c567b8dce5273cb64b2b3e18f77e604d282ad52fb7dc9f0f3bb9fde283bbab73f763fe03a15352be2c8307f7fd7bbda87849'
